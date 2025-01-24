09cd5a89c02a8b8729d59c9d59edfef2732c757a
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-24 01:04:58 +0100

    feat: add country model and test

---
+++ src/test/java/com/example/demo/model/CountryTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CountryTest {
+
+    private Country country;
+
+    @BeforeEach
+    public void setUp() {
+        this.country = new Country();
+    }
+
+    @Test
+    public void testPriceSuccess() {
+        this.country.setCountryName("France");
+        assertEquals("France", this.country.getCountryName());
+        this.country.setCountryName("England");
+        assertEquals("England", this.country.getCountryName());
+    }
+}
