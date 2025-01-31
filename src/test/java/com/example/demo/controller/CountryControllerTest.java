363bd10a132ced1bd9f9f3fe91aec56a2cfeea9a
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-31 11:12:23 +0100

    test: add AddCountryTest

--- src/test/java/com/example/demo/controller/CountryControllerTest.java
+++ src/test/java/com/example/demo/controller/CountryControllerTest.java
@@ -1,19 +1,28 @@
 package com.example.demo.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import com.example.demo.model.Country;
+import com.example.demo.repository.CountryRepository;
+import com.example.demo.request.CountryRequest;
 import com.example.demo.service.CountryService;
 
 import reactor.core.publisher.Mono;
 
+@ExtendWith(MockitoExtension.class)
 @WebFluxTest(CountryController.class)
 public class CountryControllerTest {
 
@@ -23,18 +32,44 @@ public class CountryControllerTest {
     @MockBean
     private CountryService countryService;
 
+    @MockBean
+    private CountryRepository countryRepository;
+
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
-    public void testGetCountrySuccess() {
-        when(countryService.getCountry(1)).thenReturn(Mono.just(new Country("France")));
+    public void testAddCountrySuccess() {
+        Country country = new Country("France");
 
-        webTestClient.get()
-                .uri("/country/1")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Country.class)
-                .isEqualTo(new Country("France"));
+        when(countryRepository.save(country)).thenReturn(Mono.just(country));
+        when(countryService.addCountry("France")).thenReturn(Mono.just(country));
+
+        WebTestClient.ResponseSpec response = webTestClient.post()
+                .uri("/country")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(new CountryRequest("France")))
+                .exchange();
+
+        assertEquals(201, response.returnResult(SliceImpl.class).getStatus().value());
+
     }
+
+    /*
+     * @Test
+     * public void testGetCountrySuccess() {
+     * Country country = new Country("France");
+     * 
+     * when(countryService.getCountry(1)).thenReturn(Mono.just(country));
+     * 
+     * webTestClient.get()
+     * .uri("/country/1")
+     * .exchange()
+     * .expectStatus().isOk()
+     * .expectBody(Country.class)
+     * .isEqualTo(new Country("France"));
+     * 
+     * Mockito.verify(countryRepository.save(country))
+     * }
+     */
 }
